475cde68277a09dd46169466544d2d047313b250
ghansham deshmukh <ghanshamdeshmukh2003@gmail.com>
2026-06-07 23:39:42 +0530

    change in env

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 const VITE_API_URL=import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 const api = axios.create({
-  baseURL: VITE_API_URL,
+  baseURL: "https://tdc-matchmaker-mvp.onrender.com/api",
   headers: { 'Content-Type': 'application/json' }
 });
 
